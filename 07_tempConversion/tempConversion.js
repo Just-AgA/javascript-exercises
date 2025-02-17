ff91fa9c054b7d94be48ccd80dc2e4353829b4bd
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-02-17 22:56:24 +0100

    Add function body so all tests pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFhr) {
+  return parseFloat(((tempFhr - 32) * 5/9).toFixed(1))
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelc) {
+  return parseFloat((tempCelc * 9/5 + 32).toFixed(1))
 };
 
 // Do not edit below this line
